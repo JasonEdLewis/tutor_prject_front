39fe8703d6cbca9310a13972bf41af162ab3fc0f
jason <jasonedlewis@gmail.com>
2019-10-13 13:21:57 -0400

    Sorted and set up new student extraction and logic. Need to set up student column on front end, display all students and instructors and need to add verifiaction for if student had been assigned

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -4,6 +4,7 @@ import NewInstructor from '../NewInstructorForm';
 import newStudentForm from '../NewStudentForm';
 import { Fragment } from 'react';
 import AdminShow from '../components/adminShow';
+import '../App.css';
 
 
 class Admin extends Component {
@@ -11,7 +12,8 @@ class Admin extends Component {
     state = {
         sessions: [],
         display: "",
-        studentInfo: ""
+        studentInfo: "",
+        newStudent: {},
 
 
     }
@@ -27,17 +29,29 @@ class Admin extends Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
+
+    makeNewStudentObj = (arr) => {
+        var newStudObj ={}
+        const keys = ["name","school_id","reason","date","hours","school","grade","sped","counselor_info","guardian","address","home_no","cell","email"]
+        for(var i = 0; i< arr.length; i++){
+            newStudObj[keys[i]] = arr[i]
+            debugger
+        }
+        return newStudObj
+    }
     handleSubmit = (e) => {
 
         e.preventDefault()
         // this.props.history.push('/newStudent')
-       
-        var rawInput = this.state.studentInfo.split(/\n/).filter(entry => entry !== "" && entry !== " ") 
-        var studentObj = rawInput.filter((attribute, index) => index % 2 === 1 )
+        
+        var rawInput = this.state.studentInfo.split(/\n/).filter(entry => entry !== "" && entry !== " ")
+        var newStudentArr = rawInput.filter((attribute, index) => index % 2 === 1)
+              
+        const newStudentObj = this.makeNewStudentObj(newStudentArr)
 
+        this.setState({studentInfo: '' })
 
-
-        console.log("Resulting student info:",studentObj)
+        console.log("Resulting student object:", newStudentObj)
 
         //         fetch('http://localhost:3000/students',{
         //         method: 'POST', 
@@ -58,15 +72,30 @@ class Admin extends Component {
 
 
     render() {
-        // console.log("Student info:", this.state.studentInfo)
+        console.log("Student info:", this.state.studentInfo)
         const sessions = this.state.sessions.map(session => (<li key={session.id} onClick={(e) => console.log(session.id)}>{session.student.name} has {session.subject} with {session.instructor.name} on   {session.date.replace(/-/g, "/")}</li>))
 
         return (
             <div>
+                <div className="students-left">
                 <AdminShow info={this.props} handleChange={this.handleChange} handleSubmit={this.handleSubmit} studentState={this.state.studentInfo} />
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+
+                
                 {sessions}
             </div>
         )
     }
 }
 export default Admin
+
+/*
+const keys =["name","school_id","reason","date","hours","school","grade","sped","counselor_info","guardian","address","home_no","cell","email"]
+
+*/
