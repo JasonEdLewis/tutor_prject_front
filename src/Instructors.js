5ac2332b8226ca4aabedc3f3e2c4cc485ce5dcf9
jason <jasonedlewis@gmail.com>
2019-11-07 17:25:30 -0500

    Added edit to instructors

--- src/Instructors.js
+++ src/Instructors.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchInstructors, deleteInstructor } from './actions/instructorActions';
+import { fetchInstructors, deleteInstructor, editInstructor } from './actions/instructorActions';
 import './css/instructor.css';
 import NewInstructorForm from './NewInstructorForm';
+import EditInstructorForm from './components/EditInstructor';
 import Emojicon from './components/emojicons'
 
 
@@ -10,9 +11,9 @@ import Emojicon from './components/emojicons'
 class Instructors extends Component {
 
     state = {
-        needForm: false,
+        needNewForm: false,
         showOneInstructor: false,
-        id: ""
+        needEditform: false
     }
 
     componentDidMount() {
@@ -26,12 +27,12 @@ class Instructors extends Component {
 
     addFormToPage = () => {
         this.setState({
-            needForm: !this.state.needForm
+            needNewForm: !this.state.needNewForm
         })
 
     }
-    editIntructor = (inst) => {
-        console.log(inst)
+    editIntructor = () => {
+        this.setState({ needEditform: !this.state.needEditform })
     }
 
     showOneStatus = (id) => {
@@ -40,22 +41,25 @@ class Instructors extends Component {
     }
     showOne = () => {
         const instuct = this.props.instructors.find(inst => inst.id === this.state.id)
-        return (<div className="instructors-card-div">
-             <span onClick={() => this.deleteInstructor(instuct.id)} id="trash-icon"> 🗑 </span><span onClick={() => this.editIntructor(instuct)} id="pen"> 🖋 </span><br/>
-            <p onClick={() => this.showOneStatus(instuct.id)} id="single-instructor-name"><strong>{instuct.name}</strong></p>
-            <p><strong>Subject: </strong>{instuct.subject}<br/></p><p><strong>available hours:</strong>{instuct.hours}</p> </div>)
+        return this.state.needEditform ? <EditInstructorForm id="edit-inst-form" instructor={instuct} removeForm={this.addFormToPage} /> :
+            (<div className="instructors-card-div">
+                <span onClick={() => this.deleteInstructor(instuct.id)} id="trash-icon"> 🗑 </span><span onClick={() => this.editIntructor(instuct)} id="pen"> 🖋 </span><br />
+                <p onClick={() => this.showOneStatus(instuct.id)} id="single-instructor-name"><strong>{instuct.name}</strong></p>
+                <p><strong>Subject: </strong>{instuct.subject}<br /></p><p><strong>available hours:</strong>{instuct.hours}</p> </div>)
     }
     render() {
         // debugger
         // console.log("Instructors props: ", this.props)
         const { instructors, history } = this.props
+        const { showOneInstructor, needEditform, needNewForm } = this.state
         const instructs = instructors.map(inst => <p onClick={() => this.showOneStatus(inst.id)} className="instructors"><strong> {inst.name} </strong><span> {Emojicon(inst.subject)}</span></p>)
 
         return (
             <div>
 
-                {this.state.showOneInstructor ? this.showOne() : instructs}
-                {this.state.needForm ? <NewInstructorForm removeForm={this.addFormToPage} /> : <button onClick={this.addFormToPage}>Add New Instructor</button>}
+                {showOneInstructor ? this.showOne() : instructs}
+                {needNewForm && <NewInstructorForm id="new-inst-form" removeForm={this.addFormToPage} />}
+                {!showOneInstructor && !needNewForm && <button onClick={this.addFormToPage}>Add New Instructor</button>}
             </div>
         )
     }
@@ -63,4 +67,4 @@ class Instructors extends Component {
 const mapStateToProps = (state) => {
     return { instructors: state.instructors.instructors }
 }
-export default connect(mapStateToProps, { fetchInstructors, deleteInstructor })(Instructors)
+export default connect(mapStateToProps, { fetchInstructors, deleteInstructor, editInstructor })(Instructors)
